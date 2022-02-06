8b997fe217f316eced1f4132a01bc6baa43030fe
priyam125 <sinha.priyam125>
2022-02-06 18:28:17 +0530

    Header.js edited

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,7 +8,7 @@ import Header from "./Header";
 
 const Homepage = () => {
   // const [trendingData, setTrendingData] = useState(null);
-  const [searchText, setSearchText] = useState("");
+  // const [searchText, setSearchText] = useState("");
   const trendingData = useSelector((state) => state.movie.trendingData);
 
   const dispatcher = useDispatch();
@@ -32,7 +32,7 @@ const Homepage = () => {
           type: SET_TRENDING_DATA,
           payload: { trendingData: res.data },
         });
-        setSearchText("");
+        // setSearchText("");
         console.log(res.data);
         console.log(res.data.results);
       });
@@ -45,60 +45,45 @@ const Homepage = () => {
     console.log(trendingData);
   }, []);
 
-  useEffect(() => {
-    handleSearch();
-  }, [searchText]);
+  // useEffect(() => {
+  //   handleSearch();
+  // }, [searchText]);
 
-  const handleSearch = () => {
-    console.log(searchText);
+  // const handleSearch = () => {
+  //   console.log(searchText);
 
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=2bd0ea0352bda7fcda09e90608b34039&query=${searchText}`
-      )
-      .then((res) => {
-        console.log(res.data.results);
-        dispatcher({
-          type: SET_TRENDING_DATA,
-          payload: { trendingData: res.data },
-        });
-        // setSearchText("");
-      });
-  };
+  //   axios
+  //     .get(
+  //       `https://api.themoviedb.org/3/search/movie?api_key=2bd0ea0352bda7fcda09e90608b34039&query=${searchText}`
+  //     )
+  //     .then((res) => {
+  //       console.log(res.data.results);
+  //       dispatcher({
+  //         type: SET_TRENDING_DATA,
+  //         payload: { trendingData: res.data },
+  //       });
+  //       // setSearchText("");
+  //     });
+  // };
 
-  const handleRefresh = () => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/trending/movie/day?api_key=2bd0ea0352bda7fcda09e90608b34039"
-      )
-      .then((res) => {
-        dispatcher({
-          type: SET_TRENDING_DATA,
-          payload: { trendingData: res.data },
-        });
-        setSearchText("");
-        console.log(res.data);
-        console.log(res.data.results);
-      });
-  };
+  // const handleRefresh = () => {
+  //   axios
+  //     .get(
+  //       "https://api.themoviedb.org/3/trending/movie/day?api_key=2bd0ea0352bda7fcda09e90608b34039"
+  //     )
+  //     .then((res) => {
+  //       dispatcher({
+  //         type: SET_TRENDING_DATA,
+  //         payload: { trendingData: res.data },
+  //       });
+  //       setSearchText("");
+  //       console.log(res.data);
+  //       console.log(res.data.results);
+  //     });
+  // };
 
   return (
     <div className="w-screen h-screen overflow-x-clip">
-      {/*<div className="flex mb-4 h-12 items-center justify-between border-b-2 px-6 shadow-md">
-        <div className="flex items-center">
-          <AiOutlineSearch
-            className="h-8 w-12 cursor-pointer outline-none"
-            onClick={handleSearch}
-          />
-          <input
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Search"
-            className="bg-gray-300 md:w-80 w-48 rounded px-2 py-1"
-          ></input>
-        </div>
-        <div onClick={handleRefresh} className="mr-6"><AiFillHome /></div>
-  </div>*/}
       <Header />
       <div className="flex flex-wrap justify-evenly px-4">
         {trendingData &&
--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -2,15 +2,16 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { img_300 } from "../util";
+import Header from "./Header";
 
 const MovieInfo = () => {
   const selectedMovie = useSelector((state) => state.movie.selectedMovie);
   const [poster, setPoster] = useState();
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const [extraData, setExtraData] = useState({});
-  const [release, setRelease] = useState('');
+  const [release, setRelease] = useState("");
   const [cast, setCast] = useState([]);
-  const [actor1, setActor1] = useState('');
+  const [actor1, setActor1] = useState("");
   const [actor2, setActor2] = useState();
 
   useEffect(() => {
@@ -31,48 +32,48 @@ const MovieInfo = () => {
         console.log(res.data);
       });
 
-      axios
+    axios
       .get(
         `https://api.themoviedb.org/3/movie/${selectedMovie.id}/credits?api_key=2bd0ea0352bda7fcda09e90608b34039`
       )
       .then((res) => {
         setCast(res.data.cast);
-        setActor1(res.data.cast[0].name)
-        setActor2(res.data.cast[1].name)
+        setActor1(res.data.cast[0].name);
+        setActor2(res.data.cast[1].name);
 
         console.log(res.data.cast);
-        
-        
       });
-
   }, []);
 
   return (
-    <div className="flex md:flex-row flex-col p-4">
-      <div className="mb-4">
-        <img
-          className="rounded-t-xl info-image"
-          src={`${img_300}/${poster}`}
-          alt="new"
-        />
-      </div>
-      <div className="flex flex-col ml-4">
-        <div className="flex text-xl space-x-1">
-          <div className="font-bold">{title}</div>
-          <div className="text-gray-500">{`(${selectedMovie.vote_average})`}</div>
+    <div className="flex flex-col">
+      <Header />
+      <div className="flex md:flex-row flex-col p-4">
+        <div className="mb-4">
+          <img
+            className="rounded-t-xl info-image"
+            src={`${img_300}/${poster}`}
+            alt="new"
+          />
         </div>
-        <div className="flex ">
-          <div className="mr-1">{`${extraData.release_date} |`}</div>
-          <div>{`${extraData.runtime} mins |`}</div>
-        </div>
-        <div className="flex">
-          <div className="mr-1">{`Cast: `}</div>
-          <div>{`${actor1} | `}</div>
-          <div className="ml-1">{actor2}</div>
-        </div>
-        <div className="flex flex-col items-start justify-start">
-          <div className="mt-4">{`Tagline: ${extraData.tagline}`}</div>
-          <div className="">{`Description: ${extraData.overview}`}</div>
+        <div className="flex flex-col ml-4">
+          <div className="flex text-xl space-x-1">
+            <div className="font-bold">{title}</div>
+            <div className="text-gray-500">{`(${selectedMovie.vote_average})`}</div>
+          </div>
+          <div className="flex ">
+            <div className="mr-1">{`${extraData.release_date} |`}</div>
+            <div>{`${extraData.runtime} mins |`}</div>
+          </div>
+          <div className="flex">
+            <div className="mr-1">{`Cast: `}</div>
+            <div>{`${actor1} | `}</div>
+            <div className="ml-1">{actor2}</div>
+          </div>
+          <div className="flex flex-col items-start justify-start">
+            <div className="mt-4">{`Tagline: ${extraData.tagline}`}</div>
+            <div className="">{`Description: ${extraData.overview}`}</div>
+          </div>
         </div>
       </div>
     </div>
